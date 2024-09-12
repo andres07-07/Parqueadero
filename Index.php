c81a6b21f52d4fb936e379be45f1220de834d851
andres07-07 <andresescobarbeltran@gmail.com>
2024-09-12 12:39:47 -0500

    Index.php

--- Index.php
+++ Index.php
@@ -1,4 +1,4 @@
-<!-- Incluye los archivos de SweetAlert -->
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 
